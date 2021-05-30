9be7da3ad0a72bfe353391385b558ec428b75aff
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-05-30 16:44:42 +0300

    Kategori Islemleri

--- src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 
@@ -17,8 +19,8 @@ public class Product {
 	@Column(name="product_id")
   private int id;
 	
-	@Column(name="category_id")
-  private int categoryId;
+	//@Column(name="category_id")
+  //private int categoryId;
 	
 	@Column(name="product_name")
   private String productName;
@@ -31,68 +33,99 @@ public class Product {
 	
 	@Column(name="quantity_per_unit")
   private String quantityPerUnit;
-
+	
+   @ManyToOne
+   @JoinColumn(name="category_id")
+	private Category category;
+   
+   
+   
 	public int getId() {
 		return id;
 	}
 
 	
-	public void setId(int id) {
-		this.id = id;
-	}
 
-	public int getCategoryId() {
-		return categoryId;
-	}
-
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
-	}
-
-	public String getProductName() {
-		return productName;
-	}
-
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-
-	public double getUnitPrice() {
-		return unitPrice;
-	}
-
-	public void setUnitPrice(double unitPrice) {
-		this.unitPrice = unitPrice;
-	}
-
-	public short getUnitsInStock() {
-		return unitsInStock;
-	}
-
-	public void setUnitsInStock(short unitsInStock) {
-		this.unitsInStock = unitsInStock;
-	}
-
-	public String getQuantityPerUnit() {
-		return quantityPerUnit;
-	}
-
-	public void setQuantityPerUnit(String quantityPerUnit) {
-		this.quantityPerUnit = quantityPerUnit;
-	}
-
-	public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock,
-			String quantityPerUnit) {
-		super();
-		this.id = id;
-		this.categoryId = categoryId;
-		this.productName = productName;
-		this.unitPrice = unitPrice;
-		this.unitsInStock = unitsInStock;
-		this.quantityPerUnit = quantityPerUnit;
-	}
-  
   public Product() {}
+
+
+
+public String getProductName() {
+	return productName;
+}
+
+
+
+public void setProductName(String productName) {
+	this.productName = productName;
+}
+
+
+
+public double getUnitPrice() {
+	return unitPrice;
+}
+
+
+
+public void setUnitPrice(double unitPrice) {
+	this.unitPrice = unitPrice;
+}
+
+
+
+public short getUnitsInStock() {
+	return unitsInStock;
+}
+
+
+
+public void setUnitsInStock(short unitsInStock) {
+	this.unitsInStock = unitsInStock;
+}
+
+
+
+public String getQuantityPerUnit() {
+	return quantityPerUnit;
+}
+
+
+
+public void setQuantityPerUnit(String quantityPerUnit) {
+	this.quantityPerUnit = quantityPerUnit;
+}
+
+
+
+public Category getCategory() {
+	return category;
+}
+
+
+
+public void setCategory(Category category) {
+	this.category = category;
+}
+
+
+
+public void setId(int id) {
+	this.id = id;
+}
+
+
+
+public Product(int id, String productName, double unitPrice, short unitsInStock, String quantityPerUnit,
+		Category category) {
+	super();
+	this.id = id;
+	this.productName = productName;
+	this.unitPrice = unitPrice;
+	this.unitsInStock = unitsInStock;
+	this.quantityPerUnit = quantityPerUnit;
+	this.category = category;
+}
 
   
 }
